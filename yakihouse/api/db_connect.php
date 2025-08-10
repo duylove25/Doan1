e1cba904eeae5fa32b8256fe02e73034a903ed5e
duylove25 <duy4luxxx@gmail.com>
2025-08-10 19:37:46 +0700

    Add files via upload

--- yakihouse/api/db_connect.php
+++ yakihouse/api/db_connect.php
@@ -1,11 +1,11 @@
 <?php
-// Cấu hình kết nối cơ sở dữ liệu
+//cấu hình kết nối cơ sở dữ liệu
 define('DB_SERVER', 'localhost'); 
 define('DB_USERNAME', 'root');   
 define('DB_PASSWORD', '');       
 define('DB_NAME', 'yakihouse');  
 
-// Tạo kết nối đến cơ sở dữ liệu
+// Tạo kết nối
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 
